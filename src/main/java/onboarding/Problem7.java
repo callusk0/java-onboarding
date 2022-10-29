31569c33885ad90620a84598bc599af9be46ae0b
Your Name <you@example.com>
2022-10-29 11:10:45 +0900

    7번 리팩토링
    아직 진행중
    함수 나누고 변수명을 고치는 중

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -29,32 +29,52 @@ public class Problem7 {
         {
             if(2 < friends.get(i).size()) //2보다 크면 잘못된 값
                 break;
-            if(friends_map.get(friends.get(i).get(0)) == null ? true : false)
-            {
-                List<String> templist = new ArrayList<>();
-                templist.add(friends.get(i).get(1));
-                friends_map.put(friends.get(i).get(0), templist);
-            }
-            if(friends_map.get(friends.get(i).get(1)) == null ? true : false)
-            {
-                List<String> templist = new ArrayList<>();
-                templist.add(friends.get(i).get(0));
-                friends_map.put(friends.get(i).get(1), templist);
-            }
+                
+            String another_friend = friends.get(i).get(1);
+            String main_friend = friends.get(i).get(0);
+            add_friend_not_in_map(friends_map, another_friend, main_friend);    
             
-            
-            if(friends_map.get(friends.get(i).get(0)) != null ? true : false)
-            {
-                if(!friends_map.get(friends.get(i).get(0)).contains(friends.get(i).get(1)))
-                    friends_map.get(friends.get(i).get(0)).add(friends.get(i).get(1));
-            }
-            if(friends_map.get(friends.get(i).get(1)) != null ? true : false)
-            {
-                if(!friends_map.get(friends.get(i).get(1)).contains(friends.get(i).get(0)))
-                    friends_map.get(friends.get(i).get(1)).add(friends.get(i).get(0));
-            }
+            check_add_friend_in_map(friends_map, another_friend, main_friend);
         }
         return friends_map;
+    }
+
+    private static void add_friend_not_in_map(HashMap<String, List<String>> friends_map, String another_friend,
+            String main_friend) {
+        if(!check_friend_in_map(friends_map, main_friend))
+        {
+            addFriend(friends_map, another_friend, main_friend);
+        }
+        if(!check_friend_in_map(friends_map, another_friend))
+        {
+            addFriend(friends_map, main_friend, another_friend);
+        }
+    }
+    
+    private static void check_add_friend_in_map(HashMap<String, List<String>> friends_map, String another_friend, String main_friend) {
+        if(check_friend_in_map(friends_map, main_friend))
+        {
+            add_friend_to_map(friends_map, another_friend, main_friend);
+        }
+        if(check_friend_in_map(friends_map, another_friend))
+        {
+            add_friend_to_map(friends_map, main_friend, another_friend);
+        }
+    }
+
+    private static void add_friend_to_map(HashMap<String, List<String>> friends_map, String another_friend, String main_friend) {
+        if(!friends_map.get(main_friend).contains(another_friend))
+            friends_map.get(main_friend).add(another_friend);
+    }
+    
+    private static boolean check_friend_in_map(HashMap<String, List<String>> friends_map, String friend) {
+        return friends_map.get(friend) != null ? true : false;
+    }
+    
+    private static void addFriend(HashMap<String, List<String>> friends_map, String tobeAddedfriend, String main_friend) {
+        List<String> templist = new ArrayList<>();
+        templist.add(tobeAddedfriend);
+        friends_map.put(main_friend, templist);
     }
 
     public static List<String> make_answer(String user, HashMap<String,List<String>> friends_map, List<String> visitors){
