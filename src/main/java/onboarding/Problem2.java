47c6da862090c3e024ab2aef4728993a871a7d37
Your Name <you@example.com>
2022-10-31 09:40:50 +0900

    2번
    큐로 변경해봤으나 리스트가 더 직관적인거 같아 다시 리스트로 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -44,4 +44,66 @@ public class Problem2 {
 
         return get_answer(temp.toString());
     }
+
+    
+    // private static String get_answer(String cryptogram) {
+    //     if(cryptogram.length()<2)
+    //         return cryptogram;
+        
+    //     Queue<Character> answer_queue = new LinkedList<>();
+
+    //     make_answer_queue(cryptogram, answer_queue);
+    //     answer_queue = make_answer_string(answer_queue);
+        
+    //     StringBuilder x = new StringBuilder();
+    //     while(!answer_queue.isEmpty())
+    //     {
+    //         x.append(answer_queue.poll());
+    //     }
+
+    //     return x.toString();
+    // }
+
+    // private static Queue<Character> make_answer_string(Queue<Character> answer_queue) {
+    //     if(answer_queue.size()<2)
+    //         return answer_queue;
+    //     char a,b  = 0;
+    //     int answer_queue_size = answer_queue.size();
+
+    //     a = answer_queue.poll();
+    //     b = answer_queue.poll();
+    //     Queue<Character> x = new LinkedList<>();
+
+    //     while(true)
+    //     {
+    //         if(a != b)
+    //             x.add(a);
+    //         if(a == b)
+    //         {
+    //             while(a == b){
+    //                 if(answer_queue.isEmpty())
+    //                     break;
+    //                 b=answer_queue.poll();
+    //             }
+    //         }
+    //         if(answer_queue.isEmpty())
+    //             break;
+    //         a = b;
+    //         b=answer_queue.poll();
+
+    //     }
+    //     if (a != b)
+    //         x.add(b);
+    //     if(answer_queue_size == x.size())
+    //         return x;
+    //     return make_answer_string(x);
+    // }
+
+    // private static void make_answer_queue(String cryptogram, Queue<Character> answer_queue) {
+    //     for(int i =0;i<cryptogram.length();i++)
+    //     {
+    //         answer_queue.add(cryptogram.charAt(i));
+    //     }
+    // }
+
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -77,5 +77,4 @@ public class Problem3 {
         return digit;
     }
 
-
 }
